5ff24b1d582370a4deb3ce9987f3cde71eb14223
Matthew Chick <c.chick@unsw.edu.au>
2020-08-16 11:54:46 +1000

    php artisan ui vue, next npm install && npm run dev

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/products', 'ProductionController@index');
 Route::post('/products', 'ProductionController@store');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
